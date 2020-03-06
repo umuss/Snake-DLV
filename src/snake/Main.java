e06d18f73577bcbe234e7e2890d9675fdac69c18
Giorgio Andronico <bytecodex2@gmail.com>
2020-03-06 11:13:27 +0100

    Creata cartella encodings.

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -11,7 +11,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			SampleController sampleController = loader.getController();
+			System.out.println(sampleController);
+			sampleController.drawBackground();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/snake/SampleController.java
+++ src/snake/SampleController.java
@@ -1,5 +1,19 @@
 package snake;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+
 public class SampleController {
-	
+
+    @FXML
+    private Canvas mainCanvas;
+
+    public void drawBackground() {
+
+    	Image img = new Image("file:assets/background.png");
+		mainCanvas.getGraphicsContext2D().drawImage(img, 0, 0);
+
+    }
+    
 }
